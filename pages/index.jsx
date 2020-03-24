20d9cfe818fd2d0604eaaad9ff4e01d4401752a8
audeta35 <audeta35@gmail.com>
2020-03-24 16:16:01 +0700

    Pages : ubah ekstensi .js menjadi .jsx

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class Homepage extends Component {
+
+    render() {
+
+        return (
+
+            <Container>
+                <h2>Cuci Sofa Bekasi</h2>
+            </Container>
+        )
+    }
+}
+
+export default Homepage;
