8b800a270c885c71ddd0d7cc35f6755e802856b3
audeta35 <audeta35@gmail.com>
2020-03-25 17:54:53 +0700

    Component : kustomisasi loading komponen

--- Component/Loading.js
+++ Component/Loading.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { KingBedOutlined } from '@material-ui/icons';
 
 const Loading = () => {
 
     return (
 
-        <h2>Loading</h2>
+        <div className="bg-info" style={{position: 'fixed', width: '100%', height: '100%', }}>
+            <div className="row align-items-center h-100">
+                <div className="col-12 text-center text-white mx-auto">
+                    <KingBedOutlined style={{ fontSize: 100 }} />
+                    <h1>
+                        <b>Cuci Sofa</b>
+                    </h1>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
