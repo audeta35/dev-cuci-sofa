3d075d5fb38d5e92917036b427da3e5e1ba6f5a4
audeta35 <audeta35@gmail.com>
2020-04-07 03:21:36 +0700

    Pages: buat tampilan halaman akun

--- pages/akun.jsx
+++ pages/akun.jsx
@@ -1,9 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'next/router';
+import { Container, Divider } from '@material-ui/core';
 
 import Navbars from '../Component/Navbars';
+import cleaner from '../assets/user/cleaner.png';
+import { Row, Col, Button } from 'reactstrap';
 
 class Akun extends Component {
+
+    constructor(props) {
+
+        super(props);
+    }
+
+    componentDidMount() {
+
+        console.log('pages: akun')
+    }
 
     render() {
 
@@ -11,7 +24,47 @@ class Akun extends Component {
 
             <Fragment>
                 <Navbars navbrand="Akun Saya" />
-                <h1>Halaman Akun</h1>
+                <Container maxWidth="xs" className="bg-white">
+                    <Row>
+                        <Col className="text-center mt-5 mb-5" md={12}>
+                            <img
+                                src={cleaner}
+                                className="img-thumbnail rounded-circle"
+                                width={150}
+                            />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} className="mt-3 mb-3">
+                            <b className="text-muted">username</b>
+                            <i className="float-right text-muted">audeta sandy</i>
+                        </Col>
+
+                        <Col xs={12} className="mt-3 mb-3">
+                            <b className="text-muted">alamat email</b>
+                            <i className="float-right text-muted">audeta35@gmail.com</i>
+                        </Col>
+
+                        <Col xs={12} className="mt-3 mb-3">
+                            <b className="text-muted">nomor telepon</b>
+                            <i className="float-right text-muted">081212641384</i>
+                        </Col>
+
+                        <Col xs={12} className="mt-3 mb-3">
+                            <b className="text-muted">alamat</b>
+                            <Button className="float-right" color="info" size="sm" outline>
+                                detail
+                            </Button>
+                        </Col>
+
+                        <Col xs={12} className="mt-3 mb-5">
+                            <Button color="danger" size="sm" block outline>
+                                Keluar
+                            </Button>
+                        </Col>
+                    </Row>             
+                </Container>
             </Fragment>
         )
     }
