8900ad3cfc4bd9f6879357d3a9d4daf1a0ab04b0
GheThux <audeta35@yahoo.com>
2020-04-22 14:31:42 +0700

    Component : Modifikasi tampilan AppBars.js

--- Component/AppBars.js
+++ Component/AppBars.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { HomeOutlined, ReceiptOutlined, AccountCircleOutlined } from '@material-ui/icons';
+import { HomeTwoTone, ReceiptTwoTone, AccountCircleTwoTone } from '@material-ui/icons';
 
 import { useRouter } from 'next/router';
 
@@ -32,9 +32,9 @@ const AppBars = () => {
     return (
 
         <BottomNavigation maxWidth="xs" showLabels className="fixed-bottom shadow-sm" value={router.pathname} onChange={pushRoute}>
-            <BottomNavigationAction className={isActive('/')} label="Beranda" value="/" icon={<HomeOutlined />} />
-            <BottomNavigationAction className={isActive('/pesanan')} label="Pesanan" value="/pesanan" icon={<ReceiptOutlined />} />
-            <BottomNavigationAction className={isActive('/akun')} label="Akun" value="/akun" icon={<AccountCircleOutlined />}/>
+            <BottomNavigationAction className={isActive('/')} label="Beranda" value="/" icon={<HomeTwoTone />} />
+            <BottomNavigationAction className={isActive('/pesanan')} label="Pesanan" value="/pesanan" icon={<ReceiptTwoTone />} />
+            <BottomNavigationAction className={isActive('/akun')} label="Akun" value="/akun" icon={<AccountCircleTwoTone />}/>
         </BottomNavigation>
     )
 }
