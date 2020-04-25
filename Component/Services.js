0100f9896c98dbc15a98b3a6a626231ea4759ee7
GheThux <audeta35@yahoo.com>
2020-04-26 03:25:48 +0700

    Componen : ubah Link menjadi push

--- Component/Services.js
+++ Component/Services.js
@@ -6,21 +6,30 @@ import {
     Button, Badge,
 } from 'reactstrap';
 
+import {useRouter} from 'next/router';
+
 const Services = (props) => {
+    const router = useRouter();
+
+    const pushLink = () => {
+
+        router.push('/jasa', props)
+    }
+
     return (
         <Fragment>
-            <Link href="/produk/[slug]" as={`/produk/uid`}>
-                <Card color="light">
+            <div>
+                <Card color="light" onClick={() => pushLink()}>
 
                     <img className="card-img-top" width={100} height={90} src={props.image} alt={props.title} />
                     <span className="badge badge-info" style={{ position: "absolute" }}>
-                        <i style={{ textDecoration: "line-through" }}>Rp.50.000</i>
+                        <i style={{ textDecoration: "line-through" }}> Rp {props.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} </i>
                     </span>
 
-                    <Button color="info" className="stretched-link" size="sm" outline>Rp.35.000</Button>
+                    <Button color="info" className="stretched-link" size="sm" outline>Rp {props.dicount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} </Button>
 
                 </Card>           
-            </Link>
+            </div>
         </Fragment>
     );
 };
